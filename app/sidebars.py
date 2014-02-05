088d5caddbcce3761ef0ee29eab88a694a396933
SebastianMerz <Sebastian@sebastianmerz.com>
2014-02-05 13:49:50 -0800

    Fixed sidebar logging, it was getting too much.

--- app/sidebars.py
+++ app/sidebars.py
@@ -2,5 +2,4 @@ import urllib2, json
 PA_TWITCH = 'https://api.twitch.tv/kraken/streams?game=Planetary%20Annihilation'
 def twitch_streams():
     response = urllib2.urlopen(PA_TWITCH)
-    print response
     return json.load(response)['streams']
